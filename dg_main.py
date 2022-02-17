c128760cc82afed642c85efcd3904b546f4bc8ec
SHAH NILAY JAYESH <58663029+njshah301@users.noreply.github.com>
2022-02-18 01:33:33 +0530

    Update dg_main.py

--- dg_main.py
+++ dg_main.py
@@ -39,4 +39,12 @@ body {
         pass
     
 if __name__ == '__main__':
+    import argparse
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
+    args = parser.parse_args()
     home_page()
